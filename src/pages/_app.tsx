02c371e3de3180c12ddfa53db4508b5ca6f1906b
DaivyMorales <daivymorales7705@gmail.com>
2025-04-21 18:14:07 -0500

    feat: register ui initial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type AppType } from "next/app";
 import { Geist } from "next/font/google";
+import { Crimson_Pro } from "next/font/google";
 
 import "@/styles/globals.css";
 
@@ -7,9 +8,11 @@ const geist = Geist({
   subsets: ["latin"],
 });
 
+const IbmPlexSans = Crimson_Pro({ weight: ["400", "600", "700"], subsets: ["latin"] });
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <div className={geist.className}>
+    <div className={IbmPlexSans.className}>
       <Component {...pageProps} />
     </div>
   );
